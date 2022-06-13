39fad846b251af603a0fd743ddeb69d006fc80b3
KarinaRin <beketaeva.karina@gmail.com>
2022-06-13 22:52:42 +0300

    first commit

--- blog/settings.py
+++ blog/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-from .secret_settings import SECRET_KEY
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 import rest_framework.permissions
@@ -10,7 +11,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
